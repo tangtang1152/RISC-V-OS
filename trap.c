32f4db037f21db0bd0e38263071db3e3a7017977
tangtang1152 <3324601998@qq.com>
2026-03-27 03:55:45 +0800

    add syscall return value mechanism and test multiple ecall processing
    The syscall return value does not need to be manually moved into a0 in kernel_entry; the RISC-V ABI already places a function’s return value in a0 automatically.

--- trap.c
+++ trap.c
@@ -14,7 +14,7 @@ static void print_hex(unsigned long x) {
     print_str(buf);
 }
 
-void trap_handler(unsigned long scause, unsigned long sepc, unsigned long stval, unsigned long user_a0, unsigned long user_a7) {
+unsigned long trap_handler(unsigned long scause, unsigned long sepc, unsigned long stval, unsigned long user_a0, unsigned long user_a7) {
     print_str("\n[trap]\n");
 
     print_str("scause = ");
@@ -29,16 +29,35 @@ void trap_handler(unsigned long scause, unsigned long sepc, unsigned long stval,
     print_hex(stval);
     print_str("\n");
 
+    // scause == 8 表示触发的 ecall
     if (scause == 8) {
-        if (user_a7 == 1) {
-            putchar((char)user_a0);
-        } else if (user_a7 == 2) {
-            print_str((const char *)user_a0);
+        unsigned long retval = 0;
+
+        switch (user_a7) {
+            case 1:  // putchar
+                putchar((char)user_a0);
+                break;
+
+            case 2:  // print_str
+                print_str((const char *)user_a0);
+                break;
+
+            case 3:  // 返回一个固定值
+                retval = 'Z'; // 固定返回值 'Z'
+                break;
+
+            default:
+                retval = (unsigned long)-1;  // 未知的 syscall
+                break;
         }
 
+        // 更新 sepc，继续执行用户态的下一条指令
         w_sepc(r_sepc() + 4);
-        return;
+        
+        // 返回值通过 a0 传递
+        return retval;
     }
 
+    // 如果是其他类型的 trap，进入死循环
     while (1) { }
 }
